dc08b2033af39d42a352220209c0a48c04b26340
kikye04040 <kikye04040@gmail.com>
2024-09-25 21:51:27 +0900

    feat: dto 구현

---
+++ src/main/java/com/sparta/kakaosociallogin/dto/KakaoLoginDto.java
@@ -0,0 +1,14 @@
+package com.sparta.kakaosociallogin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Builder
+@Getter
+public class KakaoLoginDto {
+
+    public String accessToken;
+    public String refreshToken;
+}
