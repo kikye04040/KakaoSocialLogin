240c0c8375a608a21f8316c0c4523cc6323242cf
kikye04040 <kikye04040@gmail.com>
2024-09-25 21:04:28 +0900

    init commit

---
+++ src/main/java/com/sparta/kakaosociallogin/KakaoSocialLoginApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.kakaosociallogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoSocialLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakaoSocialLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/kakaosociallogin/KakaoSocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.kakaosociallogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoSocialLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
